76a2e158fbe06290b79d5f512a899caddb4918ac
emaposse <eusebiomaposse@gmail.com>
2016-07-11 09:45:49 +0200

    Add form DAO.

---
+++ mentoring-core/src/main/java/mz/org/fgh/mentoring/core/form/dao/FormDAO.java
@@ -0,0 +1,17 @@
+/*
+ * MozView Technologies, Lda. 2010 - 2016
+ */
+package mz.org.fgh.mentoring.core.form.dao;
+
+import mz.co.mozview.frameworks.core.dao.GenericDAO;
+import mz.org.fgh.mentoring.core.form.model.Form;
+
+/**
+ * @author Eusebio Jose Maposse
+ *
+ */
+
+public interface FormDAO extends GenericDAO<Form, Long> {
+	
+	public String NAME = "mz.org.fgh.mentoring.core.form.dao.FormDAO";
+}
---
+++ mentoring-core/src/main/java/mz/org/fgh/mentoring/core/form/dao/FormDAOImpl.java
@@ -0,0 +1,18 @@
+/*
+ * MozView Technologies, Lda. 2010 - 2016
+ */
+package mz.org.fgh.mentoring.core.form.dao;
+
+import mz.co.mozview.frameworks.core.dao.GenericDAOImpl;
+import mz.org.fgh.mentoring.core.form.model.Form;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Eusebio Jose Maposse
+ *
+ */
+@Repository(FormDAO.NAME)
+public class FormDAOImpl extends GenericDAOImpl<Form, Long> implements FormDAO {
+
+}
