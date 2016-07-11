73eebd39745d3f48c34ef926ed31f475b5c1675a
emaposse <eusebiomaposse@gmail.com>
2016-07-11 08:33:32 +0200

    Add Question test.

---
+++ mentoring-core/src/test/java/mz/org/fgh/mentoring/core/question/QuestionServiceTest.java
@@ -0,0 +1,36 @@
+/*
+ * MozView Technologies, Lda. 2010 - 2016
+ */
+package mz.org.fgh.mentoring.core.question;
+
+import mz.co.mozview.frameworks.core.exception.BusinessException;
+import mz.org.fgh.mentoring.core.config.AbstractSpringTest;
+
+import org.junit.Test;
+
+/**
+ * @author Stélio Moiane
+ *
+ *
+ */
+public class QuestionServiceTest extends AbstractSpringTest {
+
+
+
+	@Override
+	public void setUp() throws BusinessException {
+	}
+
+	@Test
+	public void shouldCreateQuestion() throws BusinessException {
+
+
+	}
+
+	@Test
+	public void shouldUpdateQuestion() throws BusinessException {
+
+
+	}
+
+}
