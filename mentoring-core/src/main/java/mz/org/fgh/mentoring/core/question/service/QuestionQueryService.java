b21d387053d6610f8bc120cb6f926083bea011ba
emaposse <eusebiomaposse@gmail.com>
2016-07-11 08:27:18 +0200

    Add question services.

---
+++ mentoring-core/src/main/java/mz/org/fgh/mentoring/core/question/service/QuestionQueryService.java
@@ -0,0 +1,14 @@
+/*
+ * MozView Technologies, Lda. 2010 - 2016
+ */
+package mz.org.fgh.mentoring.core.question.service;
+
+/**
+ * @author Eusebio Jose Maposse
+ *
+ */
+public interface QuestionQueryService {
+
+	String NAME = "mz.org.fgh.mentoring.core.question.service.QuestionQueryService";
+
+}
---
+++ mentoring-core/src/main/java/mz/org/fgh/mentoring/core/question/service/QuestionQueryServiceImpl.java
@@ -0,0 +1,16 @@
+/*
+ * MozView Technologies, Lda. 2010 - 2016
+ */
+package mz.org.fgh.mentoring.core.question.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Eusebio Jose Maposse
+ *
+ */
+@Service(QuestionQueryService.NAME)
+public class QuestionQueryServiceImpl	 implements
+		QuestionQueryService {
+
+}
