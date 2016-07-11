fb98e7433e152588b57bc6a5a2b3da3bab06e975
emaposse <eusebiomaposse@gmail.com>
2016-07-11 08:58:19 +0200

    Ass Sector and form.

--- mentoring-core/src/main/java/mz/org/fgh/mentoring/core/form/model/Form.java
+++ mentoring-core/src/main/java/mz/org/fgh/mentoring/core/form/model/Form.java
@@ -1,8 +1,11 @@
 package mz.org.fgh.mentoring.core.form.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 import mz.co.mozview.frameworks.core.model.GenericEntity;
 
@@ -15,5 +18,13 @@ import mz.co.mozview.frameworks.core.model.GenericEntity;
 public class Form extends GenericEntity{
 
 	private static final long serialVersionUID = 1L;
+	
+	@NotEmpty
+	@Column(name = "CODE", nullable = false, length = 50)
+	private String code;
+	
+	@NotEmpty
+	@Column(name = "CODE", nullable = false, length = 50)
+	private String name;
 
 }
