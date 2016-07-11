fc2ca3d8ccbf906caff6e49aa466a39d8563608d
emaposse <eusebiomaposse@gmail.com>
2016-07-11 08:37:31 +0200

    Add test creation.

--- mentoring-core/src/test/java/mz/org/fgh/mentoring/core/question/QuestionServiceTest.java
+++ mentoring-core/src/test/java/mz/org/fgh/mentoring/core/question/QuestionServiceTest.java
@@ -3,8 +3,15 @@
  */
 package mz.org.fgh.mentoring.core.question;
 
+import javax.inject.Inject;
+
 import mz.co.mozview.frameworks.core.exception.BusinessException;
+import mz.co.mozview.frameworks.core.fixtureFactory.EntityFactory;
+import mz.co.mozview.frameworks.core.fixtureFactory.util.TestUtil;
 import mz.org.fgh.mentoring.core.config.AbstractSpringTest;
+import mz.org.fgh.mentoring.core.fixturefactory.QuestionTemplate;
+import mz.org.fgh.mentoring.core.question.model.Question;
+import mz.org.fgh.mentoring.core.question.service.QuestionService;
 
 import org.junit.Test;
 
@@ -15,21 +22,26 @@ import org.junit.Test;
  */
 public class QuestionServiceTest extends AbstractSpringTest {
 
+	private Question question;
 
+	@Inject
+	private QuestionService questionService;
 
 	@Override
 	public void setUp() throws BusinessException {
+		question = EntityFactory.gimme(Question.class, QuestionTemplate.VALID);
 	}
 
 	@Test
 	public void shouldCreateQuestion() throws BusinessException {
+		questionService.createQuestion(getUserContext(), question);
+		TestUtil.assertCreation(this.question);
 
 
 	}
 
 	@Test
 	public void shouldUpdateQuestion() throws BusinessException {
-
 
 	}
 
