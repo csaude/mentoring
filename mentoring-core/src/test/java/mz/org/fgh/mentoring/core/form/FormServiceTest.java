c796e9e458ba7617ea15e51d5a4af000142a461b
emaposse <eusebiomaposse@gmail.com>
2016-07-11 10:13:37 +0200

    Form Service.

---
+++ mentoring-core/src/test/java/mz/org/fgh/mentoring/core/form/FormServiceTest.java
@@ -0,0 +1,38 @@
+/*
+ * MozView Technologies, Lda. 2010 - 2016
+ */
+package mz.org.fgh.mentoring.core.form;
+
+import mz.co.mozview.frameworks.core.exception.BusinessException;
+import mz.org.fgh.mentoring.core.config.AbstractSpringTest;
+
+import org.junit.Test;
+
+/**
+ * @author Eusebio Jose Maposse
+ *
+ *
+ */
+public class FormServiceTest extends AbstractSpringTest {
+
+
+	@Override
+	public void setUp() throws BusinessException {
+
+
+	}
+
+	@Test
+	public void shouldCreateForm() throws BusinessException {
+
+
+	}
+
+	@Test
+	public void shouldUpdateForm() throws BusinessException {
+
+
+
+	}
+
+}
