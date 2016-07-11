3f885c2eda898d80b30b60c19bd0a661f8e6712f
emaposse <eusebiomaposse@gmail.com>
2016-07-11 10:04:00 +0200

    Add FormQuestion entity.

---
+++ mentoring-core/src/main/java/mz/org/fgh/mentoring/core/form/model/FormQuestion.java
@@ -0,0 +1,47 @@
+package mz.org.fgh.mentoring.core.form.model;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import mz.org.fgh.mentoring.core.question.model.Question;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ * @author Eusebio Jose Maposse
+ *
+ */
+@Entity
+@Table(name = "FORMS")
+public class FormQuestion {
+
+	@NotEmpty
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "FORM_ID", nullable = false)
+	private Form form;
+
+	@NotEmpty
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "QUESTION_ID", nullable = false)
+	private Question question;
+
+	public Form getForm() {
+		return form;
+	}
+
+	public void setForm(Form form) {
+		this.form = form;
+	}
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+}
