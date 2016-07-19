30fa3cc8da215f5d8dd073a30da1e4e8c5e6394e
Stélio Moiane <steliomo@gmail.com>
2016-07-20 01:07:58 +0200

    tutor creation - persistence part

--- mentoring-integ/src/main/java/mz/org/fgh/mentoring/integ/resources/tutor/TutorResourceImpl.java
+++ mentoring-integ/src/main/java/mz/org/fgh/mentoring/integ/resources/tutor/TutorResourceImpl.java
@@ -3,6 +3,7 @@
  */
 package mz.org.fgh.mentoring.integ.resources.tutor;
 
+import javax.inject.Inject;
 import javax.ws.rs.Path;
 
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ import com.sun.jersey.api.JResponse;
 
 import mz.co.mozview.frameworks.core.exception.BusinessException;
 import mz.org.fgh.mentoring.core.tutor.model.Tutor;
+import mz.org.fgh.mentoring.core.tutor.service.TutorService;
 
 /**
  * @author Stélio Moiane
@@ -20,12 +22,15 @@ import mz.org.fgh.mentoring.core.tutor.model.Tutor;
 @Path("tutors")
 public class TutorResourceImpl implements TutorResource {
 
-	@Override
-	public JResponse<Tutor> createTutor(final Tutor tutor) throws BusinessException {
+	@Inject
+	private TutorService tutorService;
 
-		// persintence logic
+	@Override
+	public JResponse<Tutor> createTutor(final TutorBeanResource tutorBeanResource) throws BusinessException {
+
+		final Tutor tutor = this.tutorService.createTutor(tutorBeanResource.getUserContext(),
+				tutorBeanResource.getTutor());
 
 		return JResponse.ok(tutor).build();
 	}
-
 }
