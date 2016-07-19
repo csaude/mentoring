34b10a63391a201087ccd57405c5e9e063906c1f
emaposse <eusebiomaposse@gmail.com>
2016-07-19 14:20:55 +0200

    Add integ service.

--- mentoring-integ/src/main/java/mz/org/fgh/mentoring/integ/resources/tutor/TutorResource.java
+++ mentoring-integ/src/main/java/mz/org/fgh/mentoring/integ/resources/tutor/TutorResource.java
@@ -8,18 +8,18 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import com.sun.jersey.api.JResponse;
-
 import mz.co.mozview.frameworks.core.exception.BusinessException;
 import mz.org.fgh.mentoring.core.tutor.model.Tutor;
+
+import com.sun.jersey.api.JResponse;
 
 /**
  * @author Stélio Moiane
  *
  */
-public interface TutorResource {
+public interface TutorResource  {
 
-	String NAME = "mz.org.fgh.mentoring.integ.resources.tuto.TutorResource";
+	String NAME = "mz.org.fgh.mentoring.integ.resources.tutor.TutorResource";
 
 	@POST
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
