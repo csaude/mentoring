6cea6071ad9cca67fc0530145619e48267a0c5da
emaposse <eusebiomaposse@gmail.com>
2016-07-20 17:57:02 +0200

    Add tutored rest services.

---
+++ mentoring-integ/src/main/java/mz/org/fgh/mentoring/integ/resources/tutor/TutoredResource.java
@@ -0,0 +1,28 @@
+/*
+ * Friends in Global Health - FGH © 2016
+ */
+package mz.org.fgh.mentoring.integ.resources.tutor;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import mz.co.mozview.frameworks.core.exception.BusinessException;
+import mz.org.fgh.mentoring.core.tutored.model.Tutored;
+
+import com.sun.jersey.api.JResponse;
+
+/**
+ * @author Eusebio Jose Maposse
+ *
+ */
+public interface TutoredResource {
+
+	String NAME = "mz.org.fgh.mentoring.integ.resources.tuto.TutorResource";
+
+	@POST
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public JResponse<Tutored> createTutored(final TutoredBeanResource tutoredBeanResource) throws BusinessException;
+}
