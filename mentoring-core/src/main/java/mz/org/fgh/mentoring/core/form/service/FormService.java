401b3f4739b0da15df17a5ff7fe368bf4646109a
steliomo <steliomo@gmail.com>
2018-10-12 12:20:03 +0200

    MENT-157:Non Applicable as a answer to the applicability of certain competencies

--- mentoring-core/src/main/java/mz/org/fgh/mentoring/core/form/service/FormService.java
+++ mentoring-core/src/main/java/mz/org/fgh/mentoring/core/form/service/FormService.java
@@ -3,13 +3,12 @@
  */
 package mz.org.fgh.mentoring.core.form.service;
 
-import java.util.Map;
 import java.util.Set;
 
 import mz.co.mozview.frameworks.core.exception.BusinessException;
 import mz.co.mozview.frameworks.core.webservices.model.UserContext;
 import mz.org.fgh.mentoring.core.form.model.Form;
-import mz.org.fgh.mentoring.core.question.model.Question;
+import mz.org.fgh.mentoring.core.formquestion.model.FormQuestion;
 
 /**
  * @author Eusebio Jose Maposse
@@ -19,27 +18,9 @@ public interface FormService {
 
 	String NAME = "mz.org.fgh.mentoring.core.form.service.FormService";
 
-	Form createForm(final UserContext userContext, final Form form, Set<Question> questions) throws BusinessException;
+	Form createForm(final UserContext userContext, final Form form, Set<FormQuestion> formQuestions)
+	        throws BusinessException;
 
-	/**
-	 *
-	 * @param userContext
-	 * @param form
-	 * @param questions
-	 * @return
-	 * @throws BusinessException
-	 */
-	Form createForm(final UserContext userContext, final Form form, Map<Integer, Question> questions) throws BusinessException;
-
-	Form updateForm(final UserContext userContext, final Form form, Set<Question> questions) throws BusinessException;
-
-	/**
-	 *
-	 * @param userContext
-	 * @param form
-	 * @param questions
-	 * @return
-	 * @throws BusinessException
-	 */
-	Form updateForm(final UserContext userContext, final Form form, Map<Integer, Question> questions) throws BusinessException;
+	Form updateForm(final UserContext userContext, final Form form, Set<FormQuestion> formQuestions)
+	        throws BusinessException;
 }
--- mentoring-core/src/test/java/mz/org/fgh/mentoring/core/formquestion/service/FormQuestionServiceTest.java
+++ mentoring-core/src/test/java/mz/org/fgh/mentoring/core/formquestion/service/FormQuestionServiceTest.java
@@ -1,12 +1,22 @@
 package mz.org.fgh.mentoring.core.formquestion.service;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
 
 import mz.co.mozview.frameworks.core.exception.BusinessException;
 import mz.co.mozview.frameworks.core.fixtureFactory.EntityFactory;
 import mz.org.fgh.mentoring.core.config.AbstractSpringTest;
+import mz.org.fgh.mentoring.core.fixturefactory.FormQuestionTemplate;
 import mz.org.fgh.mentoring.core.fixturefactory.FormTemplate;
 import mz.org.fgh.mentoring.core.fixturefactory.ProgrammaticAreaTemplate;
-import mz.org.fgh.mentoring.core.fixturefactory.QuestionTemplate;
 import mz.org.fgh.mentoring.core.form.model.Form;
 import mz.org.fgh.mentoring.core.form.service.FormService;
 import mz.org.fgh.mentoring.core.formquestion.model.FormQuestion;
@@ -14,48 +24,47 @@ import mz.org.fgh.mentoring.core.programmaticarea.model.ProgrammaticArea;
 import mz.org.fgh.mentoring.core.programmaticarea.service.ProgrammaticAreaService;
 import mz.org.fgh.mentoring.core.question.model.Question;
 import mz.org.fgh.mentoring.core.question.service.QuestionService;
-import org.junit.Assert;
-import org.junit.Test;
-
-import javax.inject.Inject;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 
 /**
  * Created by Willa aka Baba Imu on 3/9/18.
  */
 public class FormQuestionServiceTest extends AbstractSpringTest {
-    @Inject private ProgrammaticAreaService programmaticAreaService;
-    @Inject private FormQuestionService formQuestionService;
-    @Inject private QuestionService questionService;
-    @Inject private FormService formService;
+	@Inject
+	private ProgrammaticAreaService programmaticAreaService;
+	@Inject
+	private FormQuestionService formQuestionService;
+	@Inject
+	private QuestionService questionService;
+	@Inject
+	private FormService formService;
 
-    private Set<Question> questions = new HashSet<>();
-    private Form form;
+	private final Set<Question> questions = new HashSet<>();
+	private Form form;
 
-    @Override
-    public void setUp() throws BusinessException {
-        ProgrammaticArea programmaticArea = EntityFactory.gimme(ProgrammaticArea.class, ProgrammaticAreaTemplate.VALID);
-        form = EntityFactory.gimme(Form.class, FormTemplate.VALID);
-        form.setProgrammaticArea(programmaticAreaService.createProgrammaticArea(getUserContext(), programmaticArea));
+	@Override
+	public void setUp() throws BusinessException {
+		final ProgrammaticArea programmaticArea = EntityFactory.gimme(ProgrammaticArea.class,
+		        ProgrammaticAreaTemplate.VALID);
+		this.form = EntityFactory.gimme(Form.class, FormTemplate.VALID);
+		this.form.setProgrammaticArea(
+		        this.programmaticAreaService.createProgrammaticArea(this.getUserContext(), programmaticArea));
 
-        List<Question> createdQuestions = (EntityFactory.gimme(Question.class, 10, QuestionTemplate.VALID));
-        for (Question question : createdQuestions) {
-            questionService.createQuestion(this.getUserContext(), question);
-            questions.add(question);
-        }
-        formService.createForm(getUserContext(), form, new HashSet<Question>(createdQuestions));
-    }
+		final List<FormQuestion> formQuestions = EntityFactory.gimme(FormQuestion.class, 10,
+		        FormQuestionTemplate.WITH_NO_FORM);
 
-    @Test
-    public void getFormQuestionByFormId_shouldReturnAListOfFormQuestionGivenFormId() throws BusinessException {
-        List<FormQuestion> formQuestions = formQuestionService.getFormQuestionByFormId(form.getId());
+		for (final FormQuestion formQuestion : formQuestions) {
+			this.questionService.createQuestion(this.getUserContext(), formQuestion.getQuestion());
+			this.questions.add(formQuestion.getQuestion());
+		}
 
-        assertFalse(formQuestions.isEmpty());
-        assertEquals(questions.size(), formQuestions.size());
-    }
+		this.formService.createForm(this.getUserContext(), this.form, new HashSet<>(formQuestions));
+	}
+
+	@Test
+	public void getFormQuestionByFormId_shouldReturnAListOfFormQuestionGivenFormId() throws BusinessException {
+		final List<FormQuestion> formQuestions = this.formQuestionService.getFormQuestionByFormId(this.form.getId());
+
+		assertFalse(formQuestions.isEmpty());
+		assertEquals(this.questions.size(), formQuestions.size());
+	}
 }
