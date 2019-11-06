15cb97d5d3514a0997e942c87b69c068ba4c306c
Damasceno Lopes <damasceno.lopes@fgh.org.mz>
2019-11-06 13:39:31 +0200

    Required changes to attend MENT-264

---
+++ mentoring-core/src/main/java/mz/org/fgh/mentoring/core/mentorship/model/Door.java
@@ -0,0 +1,12 @@
+package mz.org.fgh.mentoring.core.mentorship.model;
+
+/**
+ * @author damasceno.lopes
+ * 
+ * For all Mentorships of Form 'Monitoria do ATS' the Tutor should
+ * specify the door.
+ *
+ */
+public enum Door {
+    P1, P2, P3, P4
+}
---
+++ mentoring-core/src/main/java/mz/org/fgh/mentoring/core/mentorship/model/Timetable.java
@@ -0,0 +1,13 @@
+package mz.org.fgh.mentoring.core.mentorship.model;
+
+/**
+ * @author damasceno.lopes
+ * 
+ * For all Mentorships of Form 'Monitoria do ATS' on the Sector (Cabinet) 
+ * Banco de Socorros the Tutor should set the Timetable (Horário) DAY or 
+ * LATE_NIGHT (Dia ou Tarde/Noite).
+ *
+ */
+public enum Timetable {
+    DAY, LATE_NIGHT
+}
