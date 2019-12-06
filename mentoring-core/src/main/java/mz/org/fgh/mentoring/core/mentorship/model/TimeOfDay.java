23eca24a4ee16fc0cdb2ef24cfd8a51eb1741ec9
Damasceno Lopes <damasceno.lopes@fgh.org.mz>
2019-12-07 00:29:04 +0200

    Rename Timetable to TimeOfDay

--- mentoring-core/src/main/java/mz/org/fgh/mentoring/core/mentorship/model/TimeOfDay.java
+++ mentoring-core/src/main/java/mz/org/fgh/mentoring/core/mentorship/model/TimeOfDay.java
@@ -4,10 +4,10 @@ package mz.org.fgh.mentoring.core.mentorship.model;
  * @author damasceno.lopes
  * 
  * For all Mentorships of Form 'Monitoria do ATS' on the Sector (Cabinet) 
- * Banco de Socorros the Tutor should set the Timetable (Horário) DAY or 
+ * Banco de Socorros the Tutor should set the Time of Day (Horário) DAY or 
  * LATE_NIGHT (Dia ou Tarde/Noite).
  *
  */
-public enum Timetable {
+public enum TimeOfDay {
     DAY, LATE_NIGHT
 }
